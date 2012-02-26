1a140ae4aee468793477614fa4461607bf987d0e
Phil Tysoe <philtysoe@gmail.com>
2012-02-26 10:14:05 +0000

    Update submodules

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@
 mv ~/.vimrc ~/.vimrc-backup
 ln -s ~/.vim/vimrc ~/.vimrc
 
+# bundles are submodules, so grab them
+git submodule update --init
+
 # to add another bundle as a submodule use the following form:
 # git submodule add git://github.com/altercation/vim-colors-solarized.git bundle/vim-colors-solarized
